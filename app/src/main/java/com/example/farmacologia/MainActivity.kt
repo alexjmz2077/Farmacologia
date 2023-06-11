b7dbbf7a59935b424f44d94a7524f1e2351aa52f
Alex <alex.-37@hotmail.com>
2023-06-10 20:08:05 -0500

    añade texto debajo del combobox al elegir un farmaco

--- app/src/main/java/com/example/farmacologia/MainActivity.kt
+++ app/src/main/java/com/example/farmacologia/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.farmacologia
+import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
+
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,10 +23,35 @@ class MainActivity : AppCompatActivity() {
 
         autoComplete.setAdapter(adapter)
 
-        autoComplete.onItemClickListener = AdapterView.OnItemClickListener {
-                adapterView, view, i, l ->
-            val itemSelect = adapterView.getItemAtPosition(i)
-            Toast.makeText(this,"Farmacos: $itemSelect", Toast.LENGTH_SHORT).show()
+
+
+        val farmaco1: TextView = findViewById(R.id.farmaco)
+        val farmaco2: TextView = findViewById(R.id.farmaco)
+        val farmaco3: TextView = findViewById(R.id.farmaco)
+        val farmaco4: TextView = findViewById(R.id.farmaco)
+        val farmaco5: TextView = findViewById(R.id.farmaco)
+        val farmaco6: TextView = findViewById(R.id.farmaco)
+        val farmaco7: TextView = findViewById(R.id.farmaco)
+        val farmaco8: TextView = findViewById(R.id.farmaco)
+        val farmaco9: TextView = findViewById(R.id.farmaco)
+        val farmaco10: TextView = findViewById(R.id.farmaco)
+
+        autoComplete.setOnItemClickListener { adapterView, view, i, l ->
+            val itemSelect = adapterView.getItemAtPosition(i).toString()
+
+            // Aquí puedes realizar una comparación para determinar qué TextView actualizar según el fármaco seleccionado
+            when (itemSelect) {
+                "Sulfacloropiridazina" -> farmaco1.text = getString(R.string.farma1)
+                "Cefapirina sódica" -> farmaco2.text = "Farmaco seleccionado: $itemSelect"
+                "Cefalexina" -> farmaco3.text = "Farmaco seleccionado: $itemSelect"
+                "Estreptomicina" -> farmaco4.text = "Farmaco seleccionado: $itemSelect"
+                "Oxitetraciclina" -> farmaco5.text = "Farmaco seleccionado: $itemSelect"
+                "Florfenicol" -> farmaco6.text = "Farmaco seleccionado: $itemSelect"
+                "Fenbendazol" -> farmaco7.text = "Farmaco seleccionado: $itemSelect"
+                "Levamisol" -> farmaco8.text = "Farmaco seleccionado: $itemSelect"
+                "Praziquantel" -> farmaco9.text = "Farmaco seleccionado: $itemSelect"
+                "Imidocard" -> farmaco10.text = "Farmaco seleccionado: $itemSelect"
+            }
         }
     }
 }
